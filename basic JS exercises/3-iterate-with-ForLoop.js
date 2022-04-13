ada7f69109179d2fc4e16e65c58c57f8a912a715
Pieris128 <elpichichi2048@gmail.com>
2022-04-13 14:58:31 -0300

    new complete JS FCC exercises

---
+++ basic JS exercises/3-iterate-with-ForLoop.js
@@ -0,0 +1,18 @@
+//Iterate with JavaScript For Loops
+/* For loops are declared with three optional expressions separated by semicolons:
+
+for (a; b; c), where a is the initialization statement, 
+b is the condition statement, and c is the final expression
+
+*/
+
+//Use a for loop to push the values 1 through 5 onto myArray.
+
+// Setup
+const myArray = [];
+
+// Only change code below this line
+
+for (let i = 1; i < 6; i++) {
+    myArray.push(i);
+  }
